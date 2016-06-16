f4defe89affb557e98a8ee77442a2921223b043b
antikantian <antikantian@users.noreply.github.com>
2016-06-16 12:26:08 -0400

    Changed the logic of the getModel method

--- src/main/scala/co/quine/slibj/DefaultModel.scala
+++ src/main/scala/co/quine/slibj/DefaultModel.scala
@@ -1,10 +1,17 @@
 package co.quine.slibj
 
 import java.io._
-import java.nio.file.Paths
-import fs2.util.Task
+import java.nio.ByteBuffer
+import java.nio.channels._
+import java.nio.file._
+import scalaj.http._
 
 object DefaultModel {
+
+  private val homeDir = System.getProperty("user.home")
+  private val defaultModelUrl = "https://s3.amazonaws.com/models.quine.co/face_landmarks_68.dat"
+  private val defaultModelRelPath = "/.slibj/models/face_landmarks_68.dat"
+  private val defaultModelAbsPath = s"$homeDir$defaultModelRelPath"
 
   private def getTempFile(prefix: String, suffix: String) = {
     val tmp = File.createTempFile(prefix, suffix)
@@ -12,14 +19,31 @@ object DefaultModel {
     tmp
   }
 
-  def get = {
-    val tmp = getTempFile("slib", ".dat")
-    val res = getClass.getResource("/models/face_landmarks_68.dat")
-    val reader = fs2.io.file.readAll[Task](Paths.get(res.getPath), 4096)
-      .through(fs2.io.file.writeAll(Paths.get(tmp.getAbsolutePath)))
-      .run
-    reader.unsafeRun
-    tmp
+  private def modelExistsHome = {
+    val modelHomePath = Paths.get(defaultModelAbsPath)
+    Files.exists(modelHomePath)
+  }
+
+  def getModel = modelExistsHome match {
+    case true => defaultModelAbsPath
+    case false =>
+      Files.createDirectories(Paths.get(s"$homeDir/.slibj/models"))
+      val is = new ByteArrayInputStream(Http(defaultModelUrl).asBytes.body)
+      val ic = Channels.newChannel(is)
+      val os = new FileOutputStream(defaultModelAbsPath).getChannel
+      val buffer = ByteBuffer.allocate(4096)
+
+      try {
+        while (ic.read(buffer) >= 0 || buffer.position() > 0) {
+          buffer.flip()
+          os.write(buffer)
+          buffer.compact()
+        }
+      } finally {
+        is.close()
+        ic.close()
+        os.close()
+      }
+      defaultModelAbsPath
   }
 }
-
